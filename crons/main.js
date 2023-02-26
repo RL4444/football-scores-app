fbcc913f507c2727dbc494de07c3b6b48f415e32
RL4444 <lewisroryjames@gmail.com>
2023-02-26 14:18:34 +0100

    temp cron removed

--- crons/main.js
+++ crons/main.js
@@ -43,8 +43,7 @@ const fixturesUpdateJobs = cron.schedule(`*/2 * * * *`, async () => {
     }
 });
 
-// const populateTimetableJob = cron.schedule(`2 2 * * *`, async () => {
-const populateTimetableJob = cron.schedule(`17 * * * *`, async () => {
+const populateTimetableJob = cron.schedule(`2 2 * * *`, async () => {
     console.log('Starting daily timetable populate cron');
     const todaysJobs = JSON.parse(fs.readFileSync(path.join(__dirname, '/jobs.json')));
     const todaysDate = moment().format('DD-MM-yyyy');
